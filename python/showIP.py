eb5d32c7f47838570ae0ab6b0c6bfc51864b9a88
William H. Bell <whbqcd1@gmail.com>
2016-04-15 21:50:11 +0100

    Adding files

---
+++ python/showIP.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# W. H. Bell
+#
+# A python script to show the current IP address(s) on the screen
+# of a Nokia LCD display.
+#
+
+import netifaces
+import time
+from nokiaSPI import NokiaSPI
+
+#---------------------------------------------------
+
+def addresses():
+  interfaces = netifaces.interfaces()
+
+  addrs=[]
+  for i in interfaces:
+    if i == 'lo':
+      continue
+    iface = netifaces.ifaddresses(i).get(netifaces.AF_INET)
+    if iface != None:
+      for j in iface:
+        addrs += [j['addr']]
+
+  return addrs
+
+#---------------------------------------------------
+
+def fillStr(text):
+  newStr = ""
+  nchar = len(text)
+  for i in xrange(14):
+    if i < nchar:
+      newStr += text[i]
+    else:
+      newStr += " "
+  return newStr
+  
+
+#---------------------------------------------------
+
+noki = NokiaSPI((0,0),5000000, 0)
+noki.cls()
+noki.text("\x7f \x7f \x7f \x7f \x7f \x7f \x7f ")
+noki.text(fillStr("Checking the "))
+noki.text(fillStr("IP address..."))
+time.sleep(3)
+
+try:
+  while 1:
+    noki.cls()
+    addrs = addresses()
+    noki.text(fillStr("Address IP:"))
+    for a in addrs:
+      noki.text(fillStr(a))
+    time.sleep(2)
+except KeyboardInterrupt:
+  print("Exiting")
+  noki.cleanup()
+finally:
+  print("Finished")
---
+++ python/showMagPiImg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# W. H. Bell
+#
+# A python script to show the orginal MagPi logo on the screen
+# of a Nokia LCD display.
+#
+import time
+from nokiaSPI import NokiaSPI
+
+# Create a noki object
+noki = NokiaSPI()
+
+try:
+  # Load the image on the screen
+  noki.load_bitmap("../images/MagPi-nokia-bw-rotated-mc.bmp",True)
+
+  # Keep the image on the screen
+  while 1:
+    time.sleep(100)
+
+# Clean up the noki object if CTRL-C is pressed
+except KeyboardInterrupt:
+  print("Exiting")
+  noki.cleanup()
